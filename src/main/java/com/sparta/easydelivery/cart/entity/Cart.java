cf8f750625b4746a027c0d0c52b11e1970851c12
gk <iba3374@naver.com>
2023-12-05 21:22:57 +0900

    feat: 장바구니 엔티티 추가

---
+++ src/main/java/com/sparta/easydelivery/cart/entity/Cart.java
@@ -0,0 +1,33 @@
+package com.sparta.easydelivery.cart.entity;
+
+import com.sparta.easydelivery.user.entity.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Cart {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @OneToMany(mappedBy = "cart")
+    @JoinColumn
+    private List<CartProduct> cartProducts;
+
+}
