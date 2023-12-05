7fb1270913dd96542a203ada8e41a02882580057
gk <iba3374@naver.com>
2023-12-05 21:23:08 +0900

    feat: 장바구니-상품 중간 엔티티 추가

---
+++ src/main/java/com/sparta/easydelivery/cart/entity/CartProduct.java
@@ -0,0 +1,31 @@
+package com.sparta.easydelivery.cart.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CartProduct {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id", nullable = false)
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @JoinColumn(nullable = false)
+    private int quantity;
+}
