912530a293a381fd2a0ba8aa81a8952f5f3a8dd4
LeeYejin <leeyejin113@gmail.com>
2023-12-06 17:28:04 +0900

    feat: 주문 상세 조회, 전체 조회 권한 별로 수정

--- src/main/java/com/sparta/easydelivery/order/controller/OrderController.java
+++ src/main/java/com/sparta/easydelivery/order/controller/OrderController.java
@@ -4,12 +4,14 @@ import com.sparta.easydelivery.order.dto.OrderListResponseDto;
 import com.sparta.easydelivery.order.dto.OrderRequestDto;
 import com.sparta.easydelivery.order.dto.OrderResponseDto;
 import com.sparta.easydelivery.order.service.OrderService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +26,7 @@ public class OrderController {
 
     @PostMapping
     public ResponseEntity<OrderResponseDto> createOrder(
-        @RequestBody OrderRequestDto orderRequestDto,
+        @RequestBody @Valid OrderRequestDto orderRequestDto,
         @AuthenticationPrincipal UserDetils userDetils
     ){
         OrderResponseDto responseDto = orderService.createOrder(orderRequestDto, userDetils.getUser());
@@ -37,6 +39,15 @@ public class OrderController {
         @AuthenticationPrincipal UserDetails userDetails
     ){
         OrderListResponseDto responseDto = orderService.getOrders(userDetails.getUser());
+        return ResponseEntity.status(HttpStatus.OK).body(responseDto);
+    }
+
+    @GetMapping("/{orderId}")
+    public ResponseEntity<OrderResponseDto> getOrder(
+        @PathVariable Long orderId,
+        @AuthenticationPrincipal UserDetails userDetails
+    ){
+        OrderResponseDto responseDto = orderService.getOrder(orderId, userDetails.getUser());
         return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
 
--- src/main/java/com/sparta/easydelivery/order/service/OrderService.java
+++ src/main/java/com/sparta/easydelivery/order/service/OrderService.java
@@ -6,9 +6,12 @@ import com.sparta.easydelivery.order.dto.OrderListResponseDto;
 import com.sparta.easydelivery.order.dto.OrderRequestDto;
 import com.sparta.easydelivery.order.dto.OrderResponseDto;
 import com.sparta.easydelivery.order.entity.Order;
+import com.sparta.easydelivery.order.exception.NotFoundOrderException;
+import com.sparta.easydelivery.order.exception.NotMatchUserException;
 import com.sparta.easydelivery.order.exception.OrderNothingException;
 import com.sparta.easydelivery.order.repository.OrderRepository;
 import com.sparta.easydelivery.user.entity.User;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -33,13 +36,38 @@ public class OrderService {
         }
         orderProductService.ConvertCartToOrderProduct(cartList, order); //장바구니에 있는 상품을 ProductOrder에 옮겨 저장
         order.setTotalPrice(cartList); // 장바구니 상품 가격*수량의 합계를 총가격에 저장
+        cartService.clearCart(user); // 장바구니 비우기
+
         Order saveOrder = orderRepository.save(order);
 
         return new OrderResponseDto(saveOrder);
     }
 
     public OrderListResponseDto getOrders(User user){
-        List<Order> orderList = orderRepository.findAllByUser(user);
+        List<Order> orderList = new ArrayList<>();
+        if(user.getRole().equals(UserRoleEnum.USER)) {  // 사용자가 조회한 경우
+            orderList = orderRepository.findAllByUser(user);
+
+        }
+        else{
+            orderList = orderRepository.findAll(); // 관리자가 조회한 경우
+        }
+
         return new OrderListResponseDto(orderList);
+    }
+
+    public OrderResponseDto getOrder(Long orderId, User user){
+        Order order = getOrderEntity(orderId, user);
+        return new OrderResponseDto(order);
+    }
+
+    private Order getOrderEntity(Long orderId, User user) {
+        Order order = orderRepository.findById(orderId).orElseThrow(
+            () -> new NotFoundOrderException("해당 주문이 존재하지 않습니다.")
+        );
+        if(order.getUser().getLoginId().equals(user.getLoginId())){
+            throw new NotMatchUserException("해당 주문은 사용자의 주문이 아닙니다.");
+        }
+        return order;
     }
 }
