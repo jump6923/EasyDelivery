0a0ee8a099f6efeb72df9f3a6f19ebd614f47150
LeeYejin <leeyejin113@gmail.com>
2023-12-07 16:28:30 +0900

    refactor: MessageSource 적용

--- src/main/java/com/sparta/easydelivery/global_exception/ErrorCode.java
+++ src/main/java/com/sparta/easydelivery/global_exception/ErrorCode.java
@@ -1,38 +1,44 @@
 package com.sparta.easydelivery.global_exception;
 
+import jakarta.annotation.PostConstruct;
+import java.util.Arrays;
+import java.util.Locale;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
 
 @Getter
 public enum ErrorCode {
     /* FILTER */
-    TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다."),
+    TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "token.expired"),
 
     /* COMMON */
-    UNAUTHORIZED_USER(HttpStatus.UNAUTHORIZED, "권한이 없습니다."),
-    INVALIDATE_INPUT(HttpStatus.BAD_REQUEST, "입력 형식에 맞지 않습니다."),
+    UNAUTHORIZED_USER(HttpStatus.UNAUTHORIZED, "unauthorized.user"),
+    INVALIDATE_INPUT(HttpStatus.BAD_REQUEST, "invalidate.input"),
 
     /* USER */
-    NOT_FOUND_USER(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
-    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
-    INVALID_TOKEN(HttpStatus.BAD_REQUEST, "토큰이 일치하지 않습니다."),
-    DUPLICATED_USERNAME(HttpStatus.CONFLICT, "이미 존재하는 username입니다."),
+    NOT_FOUND_USER(HttpStatus.NOT_FOUND, "not.found.user"),
+    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "invalid.password"),
+    INVALID_TOKEN(HttpStatus.BAD_REQUEST, "invalid.token"),
+    DUPLICATED_USERNAME(HttpStatus.CONFLICT, "duplicated.username"),
 
     /* PRODUCT */
-    NOT_FOUND_PRODUCT(HttpStatus.NOT_FOUND, "해당 상품을 찾을 수 없습니다."),
+    NOT_FOUND_PRODUCT(HttpStatus.NOT_FOUND, "not.found.product"),
 
     /* CART */
-    NOT_FOUND_CART(HttpStatus.NOT_FOUND, "해당 장바구니를 찾을 수 없습니다."),
-    DUPLICATED_PRODUCT_IN_CART(HttpStatus.CONFLICT, "해당 상품은 이미 장바구니에 담겨 있습니다."),
+    NOT_FOUND_CART(HttpStatus.NOT_FOUND, "not.found.cart"),
+    DUPLICATED_PRODUCT_IN_CART(HttpStatus.CONFLICT, "duplicated.product.in.cart"),
 
     /* ORDER */
-    NOT_FOUND_ORDER(HttpStatus.NOT_FOUND, "해당 주문을 찾을 수 없습니다."),
-    EMPTY_CART(HttpStatus.BAD_REQUEST, "장바구니에 주문할 상품이 없습니다."),
+    NOT_FOUND_ORDER(HttpStatus.NOT_FOUND, "not.found.order"),
+    EMPTY_CART(HttpStatus.BAD_REQUEST, "empty.cart"),
 
     /* REVIEW */
-    NOT_FOUND_REVIEW(HttpStatus.NOT_FOUND, "해당 리뷰를 찾을 수 없습니다."),
-    NOT_COMPLETED_ORDER(HttpStatus.BAD_REQUEST, "아직 주문이 완료되지 않았습니다."),
-    DUPLICATED_REVIEW(HttpStatus.CONFLICT, "해당 주문에 대한 리뷰가 존재합니다.");
+    NOT_FOUND_REVIEW(HttpStatus.NOT_FOUND, "not.found.review"),
+    NOT_COMPLETED_ORDER(HttpStatus.BAD_REQUEST, "not.completed.order"),
+    DUPLICATED_REVIEW(HttpStatus.CONFLICT, "duplicated.review");
 
     private final HttpStatus code;
 
@@ -46,19 +52,19 @@ public enum ErrorCode {
     /**
      * MessageSource를 적용하는 경우 필요
      */
-//    @RequiredArgsConstructor
-//    @Component
-//    public static class ErrorReasonInjector {
-//
-//        private final MessageSource messageSource;
-//
-////		@Value("${application.locale}")
-////		private String locale;
-//
-//        @PostConstruct
-//        public void postConstruct() {
-//            Arrays.stream(ErrorCode.values())
-//                .forEach(errorcode -> errorcode.message = messageSource.getMessage(errorcode.message,null, Locale.getDefault()));
-//        }
-//    }
+    @RequiredArgsConstructor
+    @Component
+    public static class ErrorReasonInjector {
+
+        private final MessageSource messageSource;
+
+//		@Value("${application.locale}")
+//		private String locale;
+
+        @PostConstruct
+        public void postConstruct() {
+            Arrays.stream(ErrorCode.values())
+                .forEach(errorcode -> errorcode.message = messageSource.getMessage(errorcode.message,null, Locale.getDefault()));
+        }
+    }
 }
