30c092e10f5947ff23e2d35ccd1e46da7296e1f6
gk <iba3374@naver.com>
2023-12-07 22:24:30 +0900

    feat: 네이버 유저 DTO 추가

---
+++ src/main/java/com/sparta/easydelivery/user/dto/NaverUserDto.java
@@ -0,0 +1,14 @@
+package com.sparta.easydelivery.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class NaverUserDto {
+
+    private String id;
+
+    private String email;
+
+}
