70cbb533e1101e5f523bd69fa50e2457293f61c9
gk <iba3374@naver.com>
2023-12-07 10:50:57 +0900

    fix: 주문 응답 객채에 new HashMap<>() 추가

--- src/main/java/com/sparta/easydelivery/order/dto/OrderMapResponseDto.java
+++ src/main/java/com/sparta/easydelivery/order/dto/OrderMapResponseDto.java
@@ -1,7 +1,7 @@
 package com.sparta.easydelivery.order.dto;
 
 import com.sparta.easydelivery.order.entity.OrderStatusEnum;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import lombok.Getter;
@@ -9,7 +9,7 @@ import lombok.Getter;
 @Getter
 public class OrderMapResponseDto {
 
-    Map<OrderStatusEnum, List<OrderResponseDto>> orderMap;
+    Map<OrderStatusEnum, List<OrderResponseDto>> orderMap = new HashMap<>();
 
     public void setOrderToMap(OrderStatusEnum key, List<OrderResponseDto> responseDtoList){
         orderMap.put(key, responseDtoList);
